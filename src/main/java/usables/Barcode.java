6388d13be0ca51a4c87a6ccd2e198e155b2a5504
Lucero Raul <luceroraul12@gmail.com>
2022-12-03 18:32:55 -0300

    fix barcode class EAN13 to Code128

--- src/main/java/usables/Barcode.java
+++ src/main/java/usables/Barcode.java
@@ -4,14 +4,15 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.Code128Writer;
 import com.google.zxing.oned.EAN13Writer;
 
 import java.awt.image.BufferedImage;
 
 public class Barcode {
-    public static BufferedImage generateEAN13BarcodeImage(String barcodeText) throws WriterException {
-        EAN13Writer barcodeWriter = new EAN13Writer();
-        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.EAN_13, 300, 150);
+    public static BufferedImage generateForIdProduct(String barcodeText) throws WriterException {
+        Code128Writer barcodeWriter = new Code128Writer();
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.CODE_128, 300, 150);
         return MatrixToImageWriter.toBufferedImage(bitMatrix);
     }
 }
