cb989bdf3dc871b652e81cae7f9d036ad6456f04
Homitowen <pycgames@gmail.com>
2022-12-04 12:41:23 -0300

    add pdf class

---
+++ src/main/java/multi/MultiBarcodeProduct.java
@@ -0,0 +1,13 @@
+package multi;
+
+import entities.Product;
+
+import java.util.List;
+
+public class MultiBarcodeProduct {
+
+
+    public static void createPdf(List<Product> products){
+
+    }
+}
