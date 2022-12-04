7f19ff10f4e745bd27404a93dfa86843221a68be
Homitowen <pycgames@gmail.com>
2022-12-04 14:14:41 -0300

    add test

---
+++ src/test/java/multi/MultiBarcodeProductTest.java
@@ -0,0 +1,15 @@
+package multi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiBarcodeProductTest {
+
+    @Test
+    void createPdf() {
+
+
+
+    }
+}
